de7b40521abe253601f733b766a0381bab228c83
ThanhTung2104 <96606078+ThanhTung2104@users.noreply.github.com>
2021-12-24 11:23:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Marry Christmas",
+    introTitle: 'Thúy đáng yêu ngọt ngào cute phô mai que',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
